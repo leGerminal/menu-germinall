1b812bfcf7a1e41630b590b5cd6cefea8659635d
leGerminal <allinnovateur@gmail.com>
2026-02-14 18:08:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const buttons = document.querySelectorAll(".langbtn");
 
-buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    buttons.forEach(b => b.classList.remove("active"));
-    btn.classList.add("active");
-
-    const lang = btn.dataset.lang;
-    document.body.classList.toggle("lang-en", lang === "en");
-  });
-});
